242141637c0111fe5487f9329dca6e004036ae9d
xpanfy00 <74433956+xpanfy00@users.noreply.github.com>
2020-11-17 16:05:38 +0100

    1

---
+++ project_4/project_4.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct Info
+{
+    char name[25];
+    char surname[25];
+    char obor[25];
+    int age;
+};
+
+int  		  question(int, int, int);
+void 		      syto(int*, int*, int*);
